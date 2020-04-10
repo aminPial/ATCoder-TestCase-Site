2ee4f7c34302d9df45cafcd74a541169bc4ea0d8
Fxmaniac <aminpial@linearaxon.com>
2020-04-10 20:03:17 +0600

    updated

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def home():
 def home2():
     with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
         data = json.loads(url.read().decode())
-    return render_template('index.html',problem_letter=ascii_uppercase,contest_names=data['contest_name'][::-1],error=1)
+    return render_template('index.html',problem_letter=ascii_uppercase[:12],contest_names=data['contest_name'][::-1],error=1)
 
 if __name__ == '__main__':engine.run(host='0.0.0.0',debug=True,port=5022)
 
