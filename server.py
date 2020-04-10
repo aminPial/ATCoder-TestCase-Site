2002d540d53a0d0c4a88e929cc5db6dbdef6e8ef
Fxmaniac <aminpial@linearaxon.com>
2020-04-10 19:57:01 +0600

    updated

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import *
-import sys,os
 import urllib.request, json
 
 from string import ascii_uppercase
@@ -8,12 +7,12 @@ engine=Flask(__name__)
 def home():
     with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
         data = json.loads(url.read().decode())
-    return render_template('index.html',problem_letter=ascii_uppercase,contest_names=data['contest_name'],error=0)
+    return render_template('index.html',problem_letter=ascii_uppercase[:12],contest_names=data['contest_name'][::-1],error=0)
 @engine.route('/?#')
 def home2():
     with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
         data = json.loads(url.read().decode())
-    return render_template('index.html',problem_letter=ascii_uppercase,contest_names=data['contest_name'],error=1)
+    return render_template('index.html',problem_letter=ascii_uppercase,contest_names=data['contest_name'][::-1],error=1)
 
 if __name__ == '__main__':engine.run(host='0.0.0.0',debug=True,port=5022)
 
