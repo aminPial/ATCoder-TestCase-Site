6925bbca943f62ead093123afc429d3f6d925937
Fxmaniac <aminpial@linearaxon.com>
2020-04-11 10:19:55 +0600

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from flask import *
 import urllib.request, json
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://3f40025957e040ec820dc8e300d7ebb8@o361561.ingest.sentry.io/5196832",
+    integrations=[FlaskIntegration()]
+)
 
 from string import ascii_uppercase
 engine=Flask(__name__)
