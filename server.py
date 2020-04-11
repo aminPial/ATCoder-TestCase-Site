54f3b8485cbe11d7d5682ba037d064326389965c
Fxmaniac <aminpial@linearaxon.com>
2020-04-11 12:34:11 +0600

    updated

--- server.py
+++ server.py
@@ -14,12 +14,16 @@ engine=Flask(__name__)
 def home():
     with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
         data = json.loads(url.read().decode())
-    return render_template('index.html',problem_letter=ascii_uppercase[:12],contest_names=data['contest_name'][::-1],error=0)
-@engine.route('/?#')
-def home2():
-    with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
-        data = json.loads(url.read().decode())
-    return render_template('index.html',problem_letter=ascii_uppercase[:12],contest_names=data['contest_name'][::-1],error=1)
+    return render_template('index.html',contest_names=[[y.lstrip().rstrip() for y in  a.split('|')]for a in  data['contest_name']],error=0)
+@engine.route('/<e>')
+def home2(e):
+        with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
+            data = json.loads(url.read().decode())
+        if e == "/?#":
+            return render_template('index.html',contest_names=[a.split('|') for a in  data['contest_name']],error=1)
+        else:
+            return render_template('index.html',contest_names=[a.split('|') for a in  data['contest_name']],error=0)
+
 
 if __name__ == '__main__':engine.run(host='0.0.0.0',debug=True,port=5022)
 
