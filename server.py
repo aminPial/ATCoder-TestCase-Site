48961be5897538677a99835dd3321e4ee3112628
Fxmaniac <aminpial@linearaxon.com>
2020-05-06 11:21:27 +0600

    u

--- server.py
+++ server.py
@@ -12,19 +12,4 @@ from string import ascii_uppercase
 engine=Flask(__name__)
 @engine.route('/')
 def home():
-    with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
-        data = json.loads(url.read().decode())
-    return render_template('index.html',contest_names=[[y.lstrip().rstrip() for y in  a.split('|')]for a in  data['contest_name']],error=0)
-@engine.route('/<e>')
-def home2(e):
-        with urllib.request.urlopen("http://134.122.27.56:5000/contest_name") as url:
-            data = json.loads(url.read().decode())
-        if e == "/?#":
-            return render_template('index.html',contest_names=[a.split('|') for a in  data['contest_name']],error=1)
-        else:
-            return render_template('index.html',contest_names=[a.split('|') for a in  data['contest_name']],error=0)
-
-
-if __name__ == '__main__':engine.run(host='0.0.0.0',debug=True,port=5022)
-
-
+    return render_template('index.html')
